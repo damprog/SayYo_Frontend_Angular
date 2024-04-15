018891efb642aed34153c1c2d1e924e7a773237b
Snoxy <damprog.one@gmail.com>
2024-04-15 13:59:11 +0200

    Added spinner (app.component.ts)

--- SayYo_Frontend_Angular/src/app/app.component.ts
+++ SayYo_Frontend_Angular/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { SpinnerService } from './services/spinner.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'SayYo';
+
+  constructor(public spinnerService: SpinnerService) {}
+
 }
