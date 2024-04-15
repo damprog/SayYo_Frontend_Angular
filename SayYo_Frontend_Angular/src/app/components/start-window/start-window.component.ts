fe20fb40cfe8e400909d0bf500e354114e858bf8
Snoxy <damprog.one@gmail.com>
2024-04-15 14:13:55 +0200

    Start-window container

---
+++ SayYo_Frontend_Angular/src/app/components/start-window/start-window.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-start-window',
+  templateUrl: './start-window.component.html',
+  styleUrl: './start-window.component.css'
+})
+export class StartWindowComponent {
+
+}
